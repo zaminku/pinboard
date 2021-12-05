4904655aeffb47e3d58c3a49ec545904f2af38c0
zaminku <zaminkugshia@gmail.com>
2021-12-04 19:28:23 -0600

    implement masonry layout

--- frontend/components/pins/pin_index.jsx
+++ frontend/components/pins/pin_index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PinIndexItemContainer from "./pin_index_item_container";
 import PinFormContainer from "./pin_form_container";
 import { Link } from "react-router-dom";
+import Masonry from "react-masonry-css";
 
 class PinIndex extends React.Component {
     constructor(props) {
@@ -16,20 +17,39 @@ class PinIndex extends React.Component {
 
     render () {
         const {pins} = this.props;
+        const pinItems = pins.map(pin => (
+            <PinIndexItemContainer className="" pin={pin} key={pin.id} />
+        ));
+        const breakpointColumnsObj = {
+            default: 8,
+            2000: 7,
+            1500: 6,
+            1000: 5
+        };
         return (
             <div className="pin-index">
                 <Link to="/pins/new"><button className="create-pin-btn">+</button></Link>
                 <button onClick={() => this.props.openModal("createBoard")}>Create a board!</button>
                 <Link to="/boards"><button>My boards</button></Link>
                 <br />
-                <div className="grid">
+                
+                <Masonry
+                    breakpointCols={breakpointColumnsObj}
+                    className="my-masonry-grid"
+                    columnClassName="my-masonry-grid_column"
+                >
+                    {pinItems}
+                </Masonry>
+
+
+                {/* <div className="grid">
                     {
                         pins.map(pin => (
                             // pass users as a prop, so that you aren't fetching it every time you go on a pin
                             <PinIndexItemContainer className="" pin={pin} key={pin.id} />
                         ))
                     }
-                </div>
+                </div> */}
             </div>
         )
     }
